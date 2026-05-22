a0b16d6347a288dcc054d6ce4fe968bf2754c7cd
juankalebe <ojuankalebe@gmail.com>
2026-05-22 11:07:15 -0300

    video 03: parte10.c

--- pext03/videos/03/parte10.c
+++ pext03/videos/03/parte10.c
@@ -2,31 +2,25 @@
 
 
 int main() {
-    // Variáveis que simulam sensores de um controlador industrial
+    // Sensores de um controlador industrial
     int pressao = 100;
     int limite = 80;
+    int verif_pressao = pressao > limite;
+    int uno = 1;
     int emergencia_ativa = 0; // 0 para falso, 1 para verdadeiro
-    /*
-       EXPRESSÃO SEM PARÊNTESES
-       A precedência do C resolverá primeiro o '&&', depois o '>',
-       o que pode não ser o que o engenheiro pretendia.
-    */
-    int resultado_vago = emergencia_ativa && pressao > limite;
-    /*
-       EXPRESSÃO COM PARÊNTESES
-       Aqui garantimos que a comparação de pressão ocorra primeiro,
-       seguida pela lógica de emergência.
-    */
-    int resultado_seguro = emergencia_ativa || (pressao > limite);
+
+    printf("verif_pressao = %d\t\tuno = %d\t\temergencia_ativa = %d\n",verif_pressao,uno,emergencia_ativa);
+
+    int resultado_vago = emergencia_ativa && verif_pressao <= uno;
+ 
+    int resultado_seguro = (emergencia_ativa && verif_pressao) <= uno;
 
 
     printf("Resultado sem clareza: %d\n", resultado_vago);
-    printf("Resultado com parenteses (desejado): %d\n\n", resultado_seguro);
+    printf("Resultado com parenteses: %d\n\n", resultado_seguro);
 
 
     // Exemplo de calculo matematico
-    // Sem parenteses: 10 + 20 / 2 * 5 = 60
-    // Com parenteses: (10 + 20) / (2 * 5) = 3
     float calculo_ambiguo = 10 + 20 / 2 * 5;
     float calculo_claro = (10.0 + 20.0) / (2.0 * 5.0);
 
